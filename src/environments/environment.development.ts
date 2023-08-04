b0d6785d1f37867a3949fefd23e7bef13a6182be
sndvsk <aleksander.daniel.veske@ut.ee>
2023-08-05 02:51:14 +0300

    styles

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8080',
-  googleMapsApiKey: 'AIzaSyACvUV_LbkyTwOOKPu9RW2rPrQt1zXYNpI',
+  googleMapsApiKey: 'key',
 };
--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'http://fd-app.eu-north-1.elasticbeanstalk.com',
-  googleMapsApiKey: 'AIzaSyACvUV_LbkyTwOOKPu9RW2rPrQt1zXYNpI',
+  googleMapsApiKey: 'key',
 };
